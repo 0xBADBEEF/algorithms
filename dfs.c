9cefaf178bec68d6938b4b4528dfa89a2e82b414
Alexander Aksenov <alexander.aksenov@mail.ru>
2016-10-21 21:39:31 +0300

    DFS: add path length and route
    Signed-off-by: Alexander Aksenov <alexander.aksenov@mail.ru>

--- dfs.c
+++ dfs.c
@@ -6,29 +6,38 @@ enum {
 
 /* Non-recoursive implementation of Depth-First Search algorithm */
 
+struct stack_el {
+	int node;
+	int parent;
+};
 
-int stack[GRAPH_VERTICES];
+struct stack_el stack[GRAPH_VERTICES];
 int discovered[GRAPH_VERTICES];
 int cur_stack = -1;
 
-void stack_push(int vert)
+void stack_push(int vert, int parent)
 {
 	cur_stack++;
-	stack[cur_stack] = vert;
+	stack[cur_stack].node = vert;
+	stack[cur_stack].parent = parent;
 }
 
-int stack_pop(void)
+void stack_pop(int *vertp, int *parentp)
 {
 	int tmp;
 
-	if (cur_stack < 0)
-		return -1;
+	if (cur_stack < 0) {
+		*vertp = -1;
+		*parentp = -1;
+		return ;
+	}
 
-	tmp = stack[cur_stack];
-	stack[cur_stack] = -1;
+	*vertp = stack[cur_stack].node;
+	*parentp = stack[cur_stack].parent;
+
+	stack[cur_stack].node = -1;
+	stack[cur_stack].parent = -1;
 	cur_stack--;
-
-	return tmp;
 }
 
 int stack_empty(void)
@@ -42,20 +51,22 @@ int stack_empty(void)
 int dfs(int adjustment_list[GRAPH_VERTICES][GRAPH_VERTICES], int path[GRAPH_VERTICES], int start, int end)
 {
 	int node;
+	int parent;
 	int child;
 	int i;
+	int verts = 0;
 
-	stack_push(start);
+	stack_push(start, start);
 
 	while(!stack_empty()) {
 
-		node = stack_pop();
+		stack_pop(&node, &parent);
 
 		if (discovered[node] == -1) {
-			discovered[node] = 1;
+			discovered[node] = parent;
 
 			if (node == end)
-				return 1;
+				goto find_path;
 
 			i = 0;
 			do {
@@ -68,7 +79,7 @@ int dfs(int adjustment_list[GRAPH_VERTICES][GRAPH_VERTICES], int path[GRAPH_VERT
 					break;
 
 				if (discovered[child] == -1)
-					stack_push(child);
+					stack_push(child, node);
 
 				i++;
 			} while (1);
@@ -76,6 +87,26 @@ int dfs(int adjustment_list[GRAPH_VERTICES][GRAPH_VERTICES], int path[GRAPH_VERT
 	}
 
 	return 0;
+
+	// Algorithm itself ends here, now it si path search
+find_path:
+	node = end;
+	parent = discovered[end];
+
+	for (i = 0; i < GRAPH_VERTICES; i++) {
+		// Beginning is its parent itself
+		// Workaround if start = end
+		if (node == parent && i > 0)
+			break;
+
+		path[i] = node;
+		verts++;
+
+		parent = node;
+		node = discovered[node];
+	}
+
+	return verts;
 }
 
 int main(void)
@@ -98,13 +129,18 @@ int main(void)
 
 	// Init arrays
 	for (i = 0; i < GRAPH_VERTICES; i++) {
-		stack[i] = -1;
+		stack[i].node = -1;
+		stack[i].parent = -1;
 		discovered[i] = -1;
 	}
 
 	ret = dfs(adjustment_list, path, 0, 8);
 
 	printf("%d\n", ret);
+
+	for (i = 0; i < ret; i++) {
+		printf("-- %d\n", path[i]);
+	}
 
 	return 0;
 }
